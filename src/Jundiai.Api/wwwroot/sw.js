55397e6a8aa4cec693cccbac7d01ee13adcd3ef3
felipemenezes25000-spec <felipemenezes25000@gmail.com>
2026-08-26 01:05:29 -0300

    pwa: inclui portais e módulos operacionais no cache

--- src/Jundiai.Api/wwwroot/sw.js
+++ src/Jundiai.Api/wwwroot/sw.js
@@ -1,5 +1,5 @@
-const CACHE='jundiai-healthos-poc-v2';
-const ASSETS=['/','/index.html','/styles.css','/clinical.css','/app.js','/acs.html','/manifest.webmanifest'];
+const CACHE='jundiai-healthos-poc-v3';
+const ASSETS=['/','/index.html','/styles.css','/clinical.css','/app.js','/citizen.html','/operations.html','/esus.html','/acs.html','/manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',event=>{
